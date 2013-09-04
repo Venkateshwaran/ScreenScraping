35f6c959e4b5e4f8efcf24e8812f31438cc63d53
Venkateshwaran <venkateshwaran.selvaraj@gmail.com>
2013-09-04 17:39:12 +0530

    updated

--- properties.py
+++ properties.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib2
+import re
 url="http://www.99acres.com/property-in-velachery-chennai-south-ffid?"
 page=urllib2.urlopen(url)
 soup = BeautifulSoup(page.read())
-properties=soup.findAll('a',{'class':'f15'})
+properties = soup.findAll('a', title=re.compile('Bedroom'))
 for eachproperty in properties:
- print eachproperty['href'], eachproperty.string
+ print eachproperty['href']+",", eachproperty.string
