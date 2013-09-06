f7971ee1427a4383dd72f720a2ba267a6b9f1764
Venkateshwaran <venkateshwaran.selvaraj@gmail.com>
2013-09-07 00:20:39 +0530

    Initial commit

---
+++ final.sql
@@ -0,0 +1,4 @@
+INSERT INTO `property` (`date`,`Url`,`Rooms`,`place`,`PId`,`Phonenumber1`,`Phonenumber2`,`Phonenumber3`,`Typeofperson`,` Nameofperson`,`typeofproperty`,`Sq.Ft`,`PerSq.Ft`,`AdDate`,`AdYear`)
+VALUES
+ (
+2013-09-07
