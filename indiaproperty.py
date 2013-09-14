69d88ed010a5defa75a735262ce7002490d6bfad
Venkateshwaran <venkateshwaran.selvaraj@gmail.com>
2013-09-14 16:07:18 +0530

    update request india

--- indiaproperty.py
+++ indiaproperty.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib2
-url="http://www.indiaproperty.com/chennai-property-search-allresidential-properties-for-sale-in-velachery-results"
-page=urllib2.urlopen(url)
-soup = BeautifulSoup(page.read())
+import requests
+start_url = requests.get('http://www.indiaproperty.com/chennai-property-search-allresidential-properties-for-sale-in-velachery-results')
+soup = BeautifulSoup(start_url.content)
 properties = soup.findAll('a',{'class':'paddl10'})
 for eachproperty in properties:
-    print eachproperty['href']
+ print re.findall("'([a-zA-Z0-9,\s]*)'", eachproperty['onclick'])
